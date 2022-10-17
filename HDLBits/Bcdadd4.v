25e4f302e7cfe11406b1e592c45654252e2b57a6
take72 <hyes941073@gmail.com>
2022-10-17 15:45:06 +0800

    bcd4

--- HDLBits/Bcdadd4.v
+++ HDLBits/Bcdadd4.v
@@ -1,3 +1,24 @@
+
+module top_module( 
+    input [15:0] a, b,
+    input cin,
+    output cout,
+    output [15:0] sum );
+    wire [3:0] carry;
+    bcd_fadd b0(.a(a[0+:4]), .b(b[0+:4]),.cin(cin), .cout(carry[0]), .sum(sum[0+:4]));
+    bcd_fadd b1(.a(a[4+:4]), .b(b[4+:4]),.cin(carry[0]), .cout(carry[1]), .sum(sum[4+:4]));
+    bcd_fadd b2(.a(a[8+:4]), .b(b[8+:4]),.cin(carry[1]), .cout(carry[2]), .sum(sum[8+:4]));
+    bcd_fadd b3(.a(a[12+:4]), .b(b[12+:4]),.cin(carry[2]), .cout(cout), .sum(sum[12+:4]));
+endmodule
+
+
+
+
+
+/*
+
+
+
 module top_module( 
     input [15:0] a, b,
     input cin,
@@ -21,4 +42,4 @@ module bcd(input [3:0] a,b,cin,
             s=a+b+cin;
         end
     end
-endmodule
+endmodule*/
